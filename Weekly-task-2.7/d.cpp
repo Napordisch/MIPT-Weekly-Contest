4223fc5700d622b8151c2b95babf27d2a8099d9a
Napordisch <Napordisch>
2024-04-14 16:58:44 +0500

    added solution for d

---
+++ Weekly-task-2.7/d.cpp
@@ -0,0 +1,138 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::vector;
+
+template <typename T>
+void Swap(T object_a, T object_b) {
+  T buffer_object = object_a;
+  object_a = object_b;
+  object_b = buffer_object;
+}
+
+struct DisjointSetUnion {
+  DisjointSetUnion() = default;
+
+  explicit DisjointSetUnion(int size) {
+    parents.resize(size);
+    heights.resize(size, 0);
+    for (int i = 0; i < size; ++i) {
+      parents[i] = i;
+    }
+  }
+
+  vector<int> parents;
+  vector<int> heights;
+
+  int Leader(int element) {
+    if (parents[element] == element) {
+      return element;
+    }
+    parents[element] = Leader(parents[element]);
+    return parents[element];
+  }
+
+  void Unite(int element_a, int element_b) {
+    int a = Leader(element_a);
+    int b = Leader(element_b);
+    if (heights[a] > heights[b]) {  // parent — the element with < height
+      Swap(a, b);
+    }
+    heights[b] = std::max(heights[b], heights[a] + 1);
+    parents[a] = b;
+  }
+};
+
+struct Edge {
+  Edge() = default;
+  Edge(int one, int two, int w) {
+    v1 = one;
+    v2 = two;
+    weight = w;
+  }
+  int v1 = 0;
+  int v2 = 0;
+  int weight = 0;
+};
+
+struct Graph {
+  Graph() = default;
+
+  explicit Graph(int vertexes_amount, int edges_amount) {
+    amount_of_edges = edges_amount;
+    amount_of_vertexes = vertexes_amount;
+    edges.resize(amount_of_edges);
+    vertexes_DSU = DisjointSetUnion(amount_of_vertexes);
+  }
+
+  void DisplayEdges() {
+    for (unsigned int i = 0; i < edges.size(); ++i) {
+      cout << edges[i].v1 + 1 << ' ';
+      cout << edges[i].v2 + 1 << ' ';
+      cout << edges[i].weight << '\n';
+    }
+  }
+
+  int amount_of_edges = 0;
+  int amount_of_vertexes = 0;
+  vector<Edge> edges;
+  DisjointSetUnion vertexes_DSU;
+  int min_spanning_tree_weight = 0;
+
+  int Kruskal() {
+    for (unsigned int i = 0; i < edges.size(); ++i) {
+      Edge the_edge = edges[i];
+      if (vertexes_DSU.Leader(the_edge.v1) != vertexes_DSU.Leader(the_edge.v2)) {
+        vertexes_DSU.Unite(the_edge.v1, the_edge.v2);
+        min_spanning_tree_weight += the_edge.weight;
+      }
+    }
+    return min_spanning_tree_weight;
+  }
+};
+
+int main() {
+  int n = 0;
+  int m = 0;
+  Graph g;
+
+  {  // input
+    std::ifstream input_file;
+    input_file.open("input.txt");
+    std::string input;
+
+    using std::getline;
+    using std::stoi;
+
+    getline(input_file, input, ' ');
+    n = stoi(input);
+    getline(input_file, input, '\n');
+    m = stoi(input);
+
+    g = Graph(n, m);
+    for (int i = 0; i < m; ++i) {
+      int vertex_one = 0;
+      int vertex_two = 0;
+      int weight = 0;
+
+      getline(input_file, input, ' ');
+      vertex_one = stoi(input);
+
+      getline(input_file, input, ' ');
+      vertex_two = stoi(input);
+
+      getline(input_file, input, '\n');
+      weight = stoi(input);
+
+      Edge the_edge(vertex_one - 1, vertex_two - 1, weight);
+      g.edges[i] = the_edge;
+    }
+
+    input_file.close();
+  }
+
+  cout << g.Kruskal() << '\n';
+}
