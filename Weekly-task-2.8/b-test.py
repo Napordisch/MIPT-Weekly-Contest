c8993288c21718805536508504292c14dfbf58e6
Napordisch <Napordisch>
2024-04-24 20:02:53 +0500

    Removed python files

--- Weekly-task-2.8/b-test.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-n = randint(1, 50)
-print(n)
-for i in range(n):
-  print(randint(1, 1000), randint(1, 1000), randint(1, 10))
-
-for i in range(n):
-  print(randint(1, 1000), randint(1, 1000))
--- Weekly-task-8/C_test.py
+++
@@ -1,3 +0,0 @@
-from random import randint
-for i in range(50):
-  print(str(randint(0, 3600)) + ' ' + str(randint(0, 3600)) + ' ' + str(randint(0, 3600)))
