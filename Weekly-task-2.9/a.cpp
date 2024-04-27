e9483ab7a90f75b450ae6fa6ba0df8b310aa3680
Napordisch <Napordisch>
2024-04-27 22:40:44 +0500

    Half-jopped algorithm that sucks ass

--- Weekly-task-2.9/a.cpp
+++ Weekly-task-2.9/a.cpp
@@ -1,16 +1,20 @@
 #include <vector>
 #include <iostream>
+#include <climits>
+
 using std::cin;
 using std::cout;
 using std::vector;
 
 class Graph { // 0-indexed inside of the class, 1-indexed outside of the class
  public:
-  Graph(int amount_of_vertexes, int amount_of_edges) {
+  Graph(int amount_of_vertexes, int amount_of_edges, int source, int destination) {
     amount_of_vertexes_ = amount_of_vertexes;
     amount_of_edges_ = amount_of_edges;
     list_.resize(amount_of_vertexes_);
     visited_.resize(amount_of_vertexes_, false);
+    source_ = source - 1;
+    target_ = destination - 1;
   }
 
   void AddEdge(int from, int to) {
@@ -22,19 +26,38 @@ class Graph { // 0-indexed inside of the class, 1-indexed outside of the class
       cout << "from";
       cout << i + 1 << ':' << ' ';
       for (unsigned int j = 0; j < list_[i].size(); ++j) {
-        cout << list_[i][j] << ' ';
+        cout << list_[i][j] + 1 << ' ';
       }
       cout << '\n';
     }
   }
-  bool ExternalDFSCall(int start, int destination) {
-    return DFS_(start - 1, destination - 1);
-  }
 
   void FordFulkerson() {
-    vector<int> flows(amount_of_vertexes_, 0);
+    int amount_of_flows = 0;
+    vector<vector<int>> flows(amount_of_vertexes_,
+                        vector<int>(amount_of_vertexes_, 0));
     vector<int> parents(amount_of_vertexes_, -1);
-    while (DFS
+    vector<vector<int>> capacity(amount_of_vertexes_, vector<int>(amount_of_vertexes_, 1));
+    while (DFS_(source_, target_, parents)) {
+      int residual_path_capacity = INT_MAX;
+      int i = target_;
+      for (int i = target_; i != source_; i = parents[i]) {
+        if (residual_path_capacity > capacity[parents[i]][i]) {
+          residual_path_capacity = capacity[parents[i]][i];
+        }
+      }
+      for (int i = target_; i != source_; i = parents[i]) {
+        flows[parents[i]][i] = flows[parents[i]][i] + residual_path_capacity;
+        flows[i][parents[i]] = -flows[parents[i]][i];
+      }
+    }
+
+    for (int i = 0; i < amount_of_vertexes_; ++i) {
+      amount_of_flows += flows[i][target_];
+    }
+
+    cout << amount_of_flows << '\n';
+
   }
 
  private:
@@ -45,7 +68,7 @@ class Graph { // 0-indexed inside of the class, 1-indexed outside of the class
   int amount_of_edges_ = 0;
   vector<bool> visited_;
 
-  bool DFS_(int start, int destination) {
+  bool DFS_(int start, int destination, vector<int> &parents) {
     cout << start + 1 << ' ';
     visited_[start] = true;
     if (start == destination) {
@@ -53,7 +76,8 @@ class Graph { // 0-indexed inside of the class, 1-indexed outside of the class
     }
     for (int neighbour : list_[start]) {
       if (!visited_[neighbour]) {
-        if (DFS_(neighbour, destination)) {
+        if (DFS_(neighbour, destination, parents)) {
+          parents[neighbour] = start;
           return true;
         }
       }
@@ -68,7 +92,7 @@ int main() {
   int apricot_lawn = 0; // s
   int home_lawn = 0; // t
   cin >> lawns_amount >> roads_amount >> apricot_lawn >> home_lawn;
-  Graph g(lawns_amount, roads_amount);
+  Graph g(lawns_amount, roads_amount, apricot_lawn, home_lawn);
   for (int i = 0; i < roads_amount; ++i) {
     int from = 0; 
     int to = 0; 
@@ -76,8 +100,7 @@ int main() {
     g.AddEdge(from, to); // 1-indexed outside of the class, 0-indexed inside of the class
   }
   g.DisplayList();
-  cout << "DFS:\n";
-  g.ExternalDFSCall(1, 3);
+  g.FordFulkerson();
 
   return 0;
 }
