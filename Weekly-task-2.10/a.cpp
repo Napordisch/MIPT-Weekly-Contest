245011686284cdb87d8ac20dc2c8553793f98875
Napordisch <Napordisch>
2024-05-02 23:12:54 +0500

    First test success

--- Weekly-task-2.10/a.cpp
+++ Weekly-task-2.10/a.cpp
@@ -19,6 +19,7 @@ struct Point
 
 struct Segment
 {
+	Segment() = default;
 	Segment(Point a, Point b)
 	{
 		a_point = a;
@@ -47,9 +48,14 @@ struct Polygon
 {
 	void ConnectPoints ()
 	{
-		segments.clear();
+		if (!segments.empty()) {
+			segments.clear();
+		}
+		else {
+			segments.resize(points.size());
+		}
 		unsigned int amount_of_points = points.size();
-		for (unsigned int i = 0; i < amount_of_points; i += 2)
+		for (unsigned int i = 0; i < amount_of_points; ++i)
 		{
 			unsigned int from = i;
 			unsigned int to = i + 1;
@@ -57,7 +63,7 @@ struct Polygon
 			{
 				to = 0;
 			}
-			segments.emplace_back(points[from], points[to]);
+			segments[i] = {points[from], points[to]};
 		}
 		points_connected = true;
 	}	
@@ -119,6 +125,23 @@ bool RaySegmentIntersection(const Ray &the_ray, const Segment &the_segment)
 	return different_cross_product_signs_by_formula && different_angle_signs;
 }
 
+bool RayBetweenPointSegments(const Ray &r, const Point &p, const Segment &left_segment, const Segment &right_segment, bool point_on_ray = false) 
+{
+	if (!point_on_ray)
+	{
+		point_on_ray = PointOnRay(p, r);
+	}
+	if (!point_on_ray) {
+		return false;
+	}
+
+	int left_angle = (r.Direction() ^ SegmentToVector(left_segment));
+	int right_angle = (r.Direction() ^ SegmentToVector(right_segment));
+
+	return (left_angle > 0 && right_angle < 0) || 
+	       (left_angle < 0 && right_angle > 0);
+}
+
 bool PointInsidePolygon(const Point &p, Polygon &s) 
 {
 	if (!s.points_connected)
@@ -129,6 +152,7 @@ bool PointInsidePolygon(const Point &p, Polygon &s)
 
 	Ray horizontal_from_point(p, Point(p.x + 1, p.y));
 
+	// segments
 	for (size_t i = 0; i < s.segments.size(); ++i)
 	{
 		if(RaySegmentIntersection(horizontal_from_point, s.segments[i])) {
@@ -136,13 +160,29 @@ bool PointInsidePolygon(const Point &p, Polygon &s)
 		}
 	}
 	
-	// TODO: check points
-	// for (size_t i = 0; i < s.points.size(); ++i)
-	// {
-		
-	// }
+	// points
+	for (size_t i = 0; i < s.points.size(); ++i)
+	{
+		const Point &polygon_point = s.points[i];
+		bool point_on_ray = PointOnRay(polygon_point, horizontal_from_point);
+		if (point_on_ray) 
+		{
+			const Segment &right_segment = s.segments[i];
+			int left_segment_index = i - 1;
+			if (i - 1 < 0) {
+				left_segment_index = static_cast<int>(s.segments.size());
+			}
+			const Segment &left_segment = s.segments[left_segment_index];
+			if(RayBetweenPointSegments(horizontal_from_point, polygon_point, left_segment, right_segment,point_on_ray))
+			{
+				++amount_of_intersections;
+			}
+		}
+	}
 
-	return amount_of_intersections % 2 == 0;
+	cout << "amount of intersections: "<< amount_of_intersections << '\n';
+
+	return amount_of_intersections % 2 != 0;
 }
 
 void Solve()
@@ -172,8 +212,28 @@ void Solve()
 	}
 }
 
+
+void Test() 
+{
+	Point ray_start(3,0);
+	Point ray_end(3,2);
+	Ray r(ray_start, ray_end);
+
+	Point a(0,0);
+	Point b(3,5); // top
+	Point c(0,6);
+	Segment ab(a, b);
+	Segment bc(b, c);
+
+	Point d(0, 5);
+	Point e(1, 5);
+	Ray rde(d, e);
+
+	cout << "Vertical: " << RayBetweenPointSegments(r, b, ab, bc) << '\n';
+	cout << "Horizontal: " << RayBetweenPointSegments(rde, b, ab, bc) << '\n';
+}
+
 int main()
 {
 	Solve();
-	return 0;
 }
